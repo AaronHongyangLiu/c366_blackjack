178f058f282007c731138756f5d24dab98898968
tdarnett <tarnett1@mac.com>
2016-10-31 17:47:38 -0600

    new project files

--- DoubleQ.py
+++ DoubleQ.py
@@ -1,29 +1,18 @@
 import blackjack
-import numpy as np
 from pylab import *
 
-numEpisodes = 2000
-Q1 = {}
-Q2 = {}
-# initialize the two Q values:
-for i in range(181):
-    Q1[(i,0)] = 0
-    Q1[(i,1)] = 0
-    Q2[(i,0)] = 0
-    Q2[(i,1)] = 0
+Q1 = ...  # NumPy array of correct size
+Q2 = ...  # NumPy array of correct size
 
-returnSum = 0.0
-for episodeNum in range(numEpisodes):
-    G = 0
-    S = blackjack.init()
-    A = [1,0]
-    a = np.random.choice(A)
-    R,S = blackjack.sample(S,a)
-    G += R
-    while (S):
-        a = np.random.choice(A)
-        R,S = blackjack.sample(S,a)
-        G += R
-    print("Episode: ", episodeNum, "Return: ", G)
-    returnSum = returnSum + G
-print("Average return: ", returnSum/numEpisodes)
+
+def learn(alpha, eps, numTrainingEpisodes):
+    ...  # Fill in Q1 and Q2
+
+
+def evaluate(numEvaluationEpisodes):
+    returnSum = 0.0
+    for episodeNum in range(numEvaluationEpisodes):
+        G = 0
+        ...
+        returnSum = returnSum + G
+    return returnSum / numEvaluationEpisodes
--- randomPolicy.py
+++ randomPolicy.py
@@ -1,21 +1,23 @@
 import blackjack
-import numpy as np
 from pylab import *
 
-numEpisodes = 2000
 
-returnSum = 0.0
-for episodeNum in range(numEpisodes):
-    G = 0
-    S = blackjack.init()
-    A = [1,0]
-    a = np.random.choice(A)
-    R,S = blackjack.sample(S,a)
-    G += R
-    while (S):
+def run(numEpisodes):
+    returnSum = 0.0
+    for episodeNum in range(numEpisodes):
+        G = 0
+        S = blackjack.init()
+        A = [1, 0]
         a = np.random.choice(A)
-        R,S = blackjack.sample(S,a)
+        R, S = blackjack.sample(S, a)
         G += R
-    print("Episode: ", episodeNum, "Return: ", G)
-    returnSum = returnSum + G
-print("Average return: ", returnSum/numEpisodes)
+        while (S):
+            a = np.random.choice(A)
+            R, S = blackjack.sample(S, a)
+            G += R
+        print("Episode: ", episodeNum, "Return: ", G)
+        returnSum = returnSum + G
+    return returnSum / numEpisodes
+
+
+run(2000)
